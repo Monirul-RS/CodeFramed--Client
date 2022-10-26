2962fafd68d8bf60259e5d00c202d4dea9b5649f
Monirul-RS <monir030599@gmail.com>
2022-10-26 12:04:11 +0600

    committ

--- src/Pages/Courses/Course/Course.js
+++ src/Pages/Courses/Course/Course.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CourseNav from '../../Shared/CourseNav/CourseNav';
+import CourseSlide from '../../Shared/CourseSlide/CourseSlide';
 
 const Course = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
+
     return (
-        <Container>
+        <Container className='mt-5'>
             <Row>
-                <Col lg='3' >
+                <Col className='border' lg='3' >
                     <CourseNav></CourseNav>
                 </Col>
                 <Col lg='9'>
-                    <h2>Courses</h2>
+                    <div>
+                        {
+                            courses.map(course => <CourseSlide
+                                key={course.id}
+                                course={course}
+                            ></CourseSlide>
+                            )}
+                    </div>
                 </Col>
             </Row>
         </Container>
+
     );
 };
 
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createContext } from 'react';
+import { getAuth } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AuthProvider;
